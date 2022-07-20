6a0d49a9e8c893dd2e2245ceb6921ab8c21b6c6f
David Esner <david@keboola.com>
2022-07-20 11:49:14 +0200

    Live login auth

--- src/main/java/keboola/bingads/ex/client/OAuthKbcAppCodeGrant.java
+++ src/main/java/keboola/bingads/ex/client/OAuthKbcAppCodeGrant.java
@@ -7,28 +7,29 @@ import java.net.URL;
 
 import com.microsoft.bingads.ApiEnvironment;
 import com.microsoft.bingads.InternalException;
+import com.microsoft.bingads.OAuthScope;
 import com.microsoft.bingads.OAuthTokens;
 import com.microsoft.bingads.internal.OAuthWithAuthorizationCode;
 
 /**
- *
  * @author David Esner <esnerda at gmail.com>
  * @created 2016
  */
 public class OAuthKbcAppCodeGrant extends OAuthWithAuthorizationCode {
-	static {
-		try {
-			DESKTOP_REDIRECT_URL = new URL("https://login.live.com/oauth20_desktop.srf");
-		} catch (MalformedURLException e) {
-			throw new InternalException(e);
-		}
-	}
-	public static final URL DESKTOP_REDIRECT_URL;
-	private static final long TIME_RESERVE_BEFORE_TOKEN_REFRESH_IN_SECONDS = 60;
+    static {
+        try {
+            DESKTOP_REDIRECT_URL = new URL("https://login.microsoftonline.com/common/oauth2/nativeclient");
+        } catch (MalformedURLException e) {
+            throw new InternalException(e);
+        }
+    }
 
-	public OAuthKbcAppCodeGrant(String clientId, String clientSecret, OAuthTokens tokens,
-			ApiEnvironment env) {
-		super(clientId, clientSecret, DESKTOP_REDIRECT_URL, tokens.getRefreshToken(), env, true);
-		this.oAuthTokens = tokens;
-	}
+    public static final URL DESKTOP_REDIRECT_URL;
+    private static final long TIME_RESERVE_BEFORE_TOKEN_REFRESH_IN_SECONDS = 60;
+
+    public OAuthKbcAppCodeGrant(String clientId, String clientSecret, OAuthTokens tokens,
+                                ApiEnvironment env) {
+        super(clientId, clientSecret, DESKTOP_REDIRECT_URL, tokens.getRefreshToken(), env, OAuthScope.MSADS_MANAGE);
+        this.oAuthTokens = tokens;
+    }
 }
